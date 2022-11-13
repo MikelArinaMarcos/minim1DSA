874b5c8126f71600d4af546d27e60d33d21cc502
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2022-11-13 18:31:21 +0100

    Clase Usuario modificada

--- src/main/java/Usuario.java
+++ src/main/java/Usuario.java
@@ -1,2 +1,26 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Usuario {
+
+    List<Partida> listaPartida;
+
+    private final String identificador;
+
+    public Usuario(String identificador){
+        this.identificador = identificador;
+        this.listaPartida = new LinkedList<Partida>();
+    }
+
+    public void addPartida(Partida p){
+        this.listaPartida.add(p);
+    }
+
+    public List<Partida> getListaPartida(){
+        return listaPartida;
+    }
+
+    public String setIdentificador(){
+        return identificador;
+    }
 }
